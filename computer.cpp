8f5ab334ae5acdfa59e1378c21e2f94ab3549930
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 13:44:58 +0530

    power check with final code

--- computer.cpp
+++ computer.cpp
@@ -3,6 +3,38 @@ using namespace std;
 
 void computerCheck();
 
+void powerCheck()
+{
+    cout<<"\n---Power Check Started---\n";
+    cout<<"\nIs the power light on? (yes/no): \n";
+    string light;
+    cin >> light;
+    if (light == "yes") 
+    {
+        cout<<"\nPower light is on, still no sign on computer turning on\n";
+        cout<<"\nFind a specialist\n";
+    }
+    else
+    {
+        cout<<"\nPower light is off\n";
+        cout<<"\nCheck power supply\n";
+        cout<<"\nIs the power plugged to the wall? (yes/no): \n";
+        string plug;
+        cin>>plug;
+        if(plug=="yes")
+        {
+            cout<<"\nPower is plugged to the wall, still no sign of computer turning on\n";
+            cout<<"\nFind a specialist\n";
+        }
+        else
+        {
+            cout<<"\nPlug the power cord to the wall\n";
+            cout<<"\nPower cord plugged in, initiate computer check again for conformation\n";
+            computerCheck();
+        }
+    }
+}
+
 void errorCheck()
 {
     cout<<"\n---Error Check Started---\n";
@@ -41,7 +73,7 @@ void computerCheck()
     {
         cout<<"\nComputer is not turned on\n";
         cout<<"\nChecking for power issues\n";
-        //powerCheck();
+        powerCheck();
     }
 }
 
